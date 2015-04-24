b8ea88acb7dda33febbc72a515a9c649970ae47e
MoaidHathot <moaid.hathot@outlook.com>
2015-04-24 03:01:46 +0300

    Adding 'Shared' folder and LamDelException

---
+++ LamDelVisualStudio/Shared/Exceptions/LamDelException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LamDel.Shared.Exceptions
+{
+    public class LamDelException : Exception
+    {
+        public LamDelException()
+        {
+
+        }
+
+        public LamDelException(string message)
+            : base(message)
+        {
+
+        }
+
+        public LamDelException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+    }
+}
